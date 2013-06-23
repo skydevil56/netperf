32ad9174606585f1e9ec6253dcb9a20f7959f868
Vladislav <n.v.v.corp@gmail.com>
2013-06-23 21:24:14 +0400

    add function

--- parsing_netperf.py
+++ parsing_netperf.py
@@ -1,133 +1,137 @@
 #!/usr/bin/python
-# Args:
-#   1: packet length
-#   2: username
-#   3: command to run
 import sys
 import warnings
 import subprocess
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     import paramiko
-	
+
 port = 22
 hostname_s1 = '192.168.7.2'
 hostname_s2 = '192.168.8.2'
-s1_ip = '192.168.7.1'
-s2_ip = '192.168.8.1'
+LocTrafGenMachineIP = '192.168.7.1'
+RemTrafGenMachineIP = '192.168.8.1'
 username = 'root'
 password = 'russia'
-NetperfOtputs = []
-vmstat_s1 = []
-vmstat_s2 = []
-out_vmstat_s1 = []
-out_vmstat_s2 = []
-temp_s1 = []
-temp_s2 = []
+LocalFileVmstatS1 = '/root/vmstat_s1'
+RemoteFileVmstatS1 = '/root/vmstat_s1'
+LocalFileVmstatS2 = '/root/vmstat_s2'
+RemoteFileVmstatS2 = '/root/vmstat_s2'
+UDP1400 = 1400
+UDP512 = 512
+UDP64 = 64
 
-paramiko.util.log_to_file('paramiko.log')
-s1 = paramiko.SSHClient()
-s1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-s1.connect(hostname_s1, port, username, password)
-#stdin, stdout, stderr = s1.exec_command(kill_vmstat_on_s1)
-stdin, stdout, stderr = s1.exec_command('vmstat 2 -n > vmstat_s1 &')
-stdin, stdout, stderr = s1.exec_command('ps -A | grep vmstat')
-pid_vmstat_s1 = stdout.read().split()[0]
-kill_vmstat_on_s1 = "kill" + " " + str(pid_vmstat_s1)
-print "pid vmstat on s1 is", pid_vmstat_s1
-#stdin.write('\n')
+def StartVmstat (Hostname, Port, Username, Password, RemoteFile):
+    vmstat_grep = []
+    vmstat_pids = []
+    start_vmstat = "vmstat 2 -n > " + str(RemoteFile) + " &"
+    paramiko.util.log_to_file('paramiko.log')
+    s = paramiko.SSHClient()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    s.connect(Hostname, Port, Username, Password)
+    stdin, stdout, stderr = s.exec_command('ps -A | grep vmstat')
+    vmstat_grep = stdout.readlines()
+    for line in vmstat_grep:
+        vmstat_pids.append(line.split()[0])
+    if len(vmstat_pids) > 0:
+        for i in vmstat_pids:
+            stdin, stdout, stderr = s.exec_command("kill " + str(i))
+        stdin, stdout, stderr = s.exec_command(start_vmstat)
+        stdin, stdout, stderr = s.exec_command('ps -A | grep vmstat')
+        vmstat_current_pid = stdout.read().split()[0]
+    else:
+        stdin, stdout, stderr = s.exec_command(start_vmstat)
+        stdin, stdout, stderr = s.exec_command('ps -A | grep vmstat')
+        vmstat_current_pid = stdout.read().split()[0]
+    s.close()
+    return vmstat_current_pid
 
+def StopVmstat (Hostname, Port, Username, Password):
+    vmstat_grep = []
+    vmstat_pids = []
+    paramiko.util.log_to_file('paramiko.log')
+    s = paramiko.SSHClient()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    s.connect(Hostname, Port, Username, Password)
+    stdin, stdout, stderr = s.exec_command('ps -A | grep vmstat')
+    vmstat_grep = stdout.readlines()
+    for line in vmstat_grep:
+        vmstat_pids.append(line.split()[0])
+    if len(vmstat_pids) > 0:
+        for i in vmstat_pids:
+            stdin, stdout, stderr = s.exec_command("kill " + str(i))
+    s.close()
 
-s2 = paramiko.SSHClient()
-s2.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-s2.connect(hostname_s2, port, username, password)
-#stdin, stdout, stderr = s2.exec_command(kill_vmstat_on_s2)
-stdin, stdout, stderr = s2.exec_command('vmstat 2 -n > vmstat_s2 &')
-stdin, stdout, stderr = s2.exec_command('ps -A | grep vmstat')
-pid_vmstat_s2 = stdout.read().split()[0]
-kill_vmstat_on_s2 = "kill" + " " + str(pid_vmstat_s2)
-print "pid vmstat on s2 is", pid_vmstat_s2
+def UDP_test (PacketLength, RemoteHostIP, LocalHostIP):
+    NetperfOtputs = []
+    netperf = subprocess.Popen("netperf -H " + str(RemoteHostIP) + " -L " + str(LocalHostIP) + " -t UDP_STREAM" + " -i 5,5 " + "-l 20" + " -- " + " -m " + str(PacketLength) + " -M " + str(PacketLength), shell=True, executable='/bin/bash', stdout=subprocess.PIPE)
+    NetperfOtputs = netperf.stdout.readlines()
+    return NetperfOtputs[-3].split()[-1], NetperfOtputs[-2].split()[-1]
 
-print '\n'
-print "Test is running, please wait..."
+def GetVmstatFiles (Hostname, Port, Username, Password, LocalFile, RemoteFile):
+    s = paramiko.SSHClient()
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    s.connect(Hostname, Port, Username, Password)
+    ftp = s.open_sftp()
+    ftp.get(LocalFile, RemoteFile)
+    s.close()
 
-netperf = subprocess.Popen("netperf -H " + str(s2_ip) + " -L " + str(s1_ip) + " -t UDP_STREAM" + " -i 5,5 " + "-l 20" + " -- " + " -m " + str(sys.argv[1]) + " -M " + str(sys.argv[1]), shell=True, executable='/bin/bash', stdout=subprocess.PIPE)
-NetperfOtputs = netperf.stdout.readlines()
+def ParsingVmstatOutput (LocalFile):
+        vmstat_out =[]
+        cpu_load = []
+        true_cpu_load =[]
+        # open file with Vmstat output
+        file = open(LocalFile, 'r')
+        vmstat_out = file.readlines()
+        #head not read
+        for line in vmstat_out[3:-1:]:
+                #parsing CPU (numbers) load and add their to list
+                cpu_load.append(float(line.split()[13]))
 
-ftp_s1 = s1.open_sftp()
-ftp_s1.get('/root/vmstat_s1', '/root/vmstat_s1')
-ftp_s2 = s2.open_sftp()
-ftp_s2.get('/root/vmstat_s2', '/root/vmstat_s2')
+        #delete zeros and ones
+        if cpu_load.count(0.0) != 0:
+                count_zeros = cpu_load.count(0.0)
+                for i in range(count_zeros):
+                        cpu_load.remove(0.0)
 
-#beegin //////////////parsing CPU load
+        if cpu_load.count(1.0) != 0:
+                count_ones = cpu_load.count(1.0)
+                for i in range(count_ones):
+                        cpu_load.remove(1.0)
 
-# open file vmstat_s1
-file_s1 = open('vmstat_s1', 'r')
-vmstat_s1 = file_s1.readlines()
-#head not read
-for line in vmstat_s1[3:-1:]:
-#parsing CPU load and add to list
-    temp_s1.append(float(line.split()[13]))
+        #find approximate average of CPU load
+        avg = sum(cpu_load)/float(len(cpu_load))
 
-#delete zeros ans ones
-if temp_s1.count(0.0) != 0:
-    for i in range(temp_s1.count(0.0)):
-        temp_s1.remove(0.0)
+        #create new list where items > approximate average
+        for i in cpu_load:
+                if i > avg:
+                        true_cpu_load.append(i)
+        return sum(true_cpu_load)/float(len(true_cpu_load))
 
-if temp_s1.count(1.0) != 0:
-    for i in range(temp_s1.count(1.0)):
-        temp_s1.remove(1.0)
+def GeneralUDPTest (PacketLength, RemoteTrafficGeneratorMachineIP, LocalTrafficGeneratorMachineIP):
+        #Start Vmstat
+        vmstat_current_pid_os_s1 = StartVmstat(hostname_s1, port, username, password, RemoteFileVmstatS1)
+        vmstat_current_pid_on_s2 = StartVmstat(hostname_s2, port, username, password, RemoteFileVmstatS2)
+        #print vmstat_current_pid_os_s1
+        #print vmstat_current_pid_on_s2
+        #Start Netperf test
+        MaxSpeed, SpeedWithoutLoss = UDP_test (PacketLength, RemoteTrafficGeneratorMachineIP, LocalTrafficGeneratorMachineIP)
+        print "Test UDP, PacketLength is ", PacketLength
+        print MaxSpeed, "Throughput max 10^6bits/sec"
+        print SpeedWithoutLoss, "Throughput without loss 10^6bits/sec"
+        #Get Vmstat files
+        GetVmstatFiles(hostname_s1, port, username, password, LocalFileVmstatS1, RemoteFileVmstatS1)
+        GetVmstatFiles(hostname_s2, port, username, password, LocalFileVmstatS2, RemoteFileVmstatS2)
+        #Parse CPU load
+        cpu_load_on_site1 = ParsingVmstatOutput(LocalFileVmstatS1)
+        cpu_load_on_site2 = ParsingVmstatOutput(LocalFileVmstatS2)
+        print cpu_load_on_site1, "CPU load on Site_1"
+        print cpu_load_on_site2, "CPU load on Site_2"
+        print
+        #Stop Vmstat
+        StopVmstat (hostname_s1, port, username, password)
+        StopVmstat (hostname_s2, port, username, password)
 
-#find average
-avg = sum(temp_s1)/float(len(temp_s1))
-
-#create new list where items > avg
-for i in temp_s1:
-    if i > avg:
-        out_vmstat_s1.append(i) 
-
-# open file vmstat_s2
-file_s2 = open('vmstat_s2', 'r')
-vmstat_s2 = file_s2.readlines()
-#head not read
-for line in vmstat_s2[3:-1:]:
-#parsing CPU load and add to list
-    temp_s2.append(float(line.split()[13]))
-
-#delete zeros ans ones
-if temp_s2.count(0.0) != 0:
-    for i in range(temp_s2.count(0.0)):
-        temp_s2.remove(0.0)
-
-if temp_s2.count(1.0) != 0:
-    for i in range(temp_s2.count(1.0)):
-        temp_s2.remove(1.0)
-
-#find average
-avg = sum(temp_s2)/float(len(temp_s2))
-
-#create new list where items > avg
-for i in temp_s2:
-    if i > avg:
-        out_vmstat_s2.append(i) 
-
-#end //////////////parsing CPU load
-
-print '\n'
-print NetperfOtputs[-3].split()[-1], "Throughput max 10^6bits/sec"
-print NetperfOtputs[-2].split()[-1], "Throughput without loss 10^6bits/sec"
-
-print '\n'
-#print out_vmstat_s1
-print sum(out_vmstat_s1)/float(len(out_vmstat_s1)), "CPU load on Site_1"
-#print out_vmstat_s2
-print sum(out_vmstat_s2)/float(len(out_vmstat_s2)), "CPU load on Site_2"
-
-stdin, stdout, stderr = s1.exec_command(kill_vmstat_on_s1)
-stdin, stdout, stderr = s2.exec_command(kill_vmstat_on_s2)
-
-file_s1.close()
-s1.close()
-s2.close()
-ftp_s1.close()
-ftp_s2.close()
+GeneralUDPTest (UDP1400, RemTrafGenMachineIP, LocTrafGenMachineIP)
+GeneralUDPTest (UDP512, RemTrafGenMachineIP, LocTrafGenMachineIP)
+GeneralUDPTest (UDP64, RemTrafGenMachineIP, LocTrafGenMachineIP)
